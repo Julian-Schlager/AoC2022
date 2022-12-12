1cace4c16a569271ff4c6abf8379fd4f6abd15ed
Schlager Julian <Julian.SCHLAGER@htlvb.at>
2022-12-12 09:15:20 +0100

    Solved day 1 part 1

--- AoC2022Day01/Program.cs
+++ AoC2022Day01/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input = File.ReadAllText("C:/Users/Julian.Schlager/OneDrive - HTL Vöcklabruc/HTL VB/Swp/5AHWII/AoC2022/input.txt");
+
+int maxCalories = input.Split("\r\n\r\n").Max(elf => elf.Split("\r\n").Sum(int.Parse));
+
+Console.WriteLine(maxCalories.ToString());
